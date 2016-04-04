e6d9fb75267e6b4abdfcbf20fe1b98837e67351b
BJ Swope <bigblueswope+github+public@gmail.com>
2016-04-04 14:36:29 -0400

    more flags in cli_parser, feed_report_update now toggles the value

--- feed_report_update.py
+++ feed_report_update.py
@@ -16,12 +16,20 @@ def main():
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
         report = cb.feed_report_info(args.feedid, args.reportid)
-    pprint.pprint(report)
+    
     url = "%s/api/v1/threat_report" % (args.url)
     
     updated_report = {'ids': {}, 'updates': {}}
     updated_report['ids'][report['feed_id']] = [report['id']]
-    updated_report['updates']['is_ignored'] = 'True'
+    
+    if report['is_ignored'] == True:
+        updated_report['updates']['is_ignored'] = False
+    elif report['is_ignored'] == False:
+        updated_report['updates']['is_ignored'] = True
+    else:
+        print "Feed: %s, Threat Report: %s 'is_ignored' was neither True nor False.  Exiting the script." % (report['feed_id'], report['id'])
+        sys.exit(1)
+        
   
     r = cb.cbapi_post(url, data=json.dumps(updated_report))
     r.raise_for_status()
