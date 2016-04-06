3dbe8ed8e64aca7d06ef39feff4c0daa0890cb06
BJ Swope <bigblueswope+github+public@gmail.com>
2016-04-06 12:40:55 -0400

    cleaned up alert_iterator.py

--- alert_iterator.py
+++ alert_iterator.py
@@ -1,13 +1,8 @@
 #!/bin/env python
 
-import sys, argparse, pprint, json, cbapi
+import sys, argparse, pprint, json, cbapi, warnings
 from cli_parser_tnc import build_cli_parser
-#sys.path= ['../cbapi/client_apis/python/src/cbapi'] + sys.path
-#import cbapi
  
-for i in sys.path:
-    print i
-
 def main():
     args = build_cli_parser()
     cb = cbapi.CbApi(args.url, token=args.token, ssl_verify=args.ssl_verify, ignore_system_proxy=True)
@@ -15,20 +10,14 @@ def main():
     if not args.url or not args.token or (args.query is None and not args.list_fields):
         print "Missing required param; run with --help for usage"
         sys.exit(-1)
-    if args.list_fields:
-        #ignore any query on the command line and set the query to look for processes that started in the last 7 days
-        #(because newer versions of CB may have different fields than what was available on older data)
-        args.query='start:-10080m'
-        #ignore the number of rows from the command line and set the row count to 1
-        args.rows=1
-        processes = cb.process_search(args.query, rows=args.rows, facet_enable=args.facet_enable)
-        print "List of fields available to be returned by this script:"
-        for process in processes['results']:
-            for k in sorted(process.iterkeys()):
-                print k
-        sys.exit(0)
  
-    processes = cb.alert_search_iter(args.query)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        print args.query
+        processes = cb.alert_search(args.query)
+        #processes = cb.alert_search_iter(args.query)
+        print "@@@@@@@@@@@"
+        print processes
     for proc in processes:
         #for m in proc[0]:
         #    p0.append(m)
