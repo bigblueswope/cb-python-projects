abdfa78bfd05f3532fa243821f26367e67b67015
BJ Swope <bigblueswope+github+public@gmail.com>
2016-04-06 17:20:48 -0400

    updated script to look for process end times via the parent process

--- powershell_long_running_yesterday.py
+++ powershell_long_running_yesterday.py
@@ -1,36 +1,59 @@
 #!/bin/env python
-import datetime, sys, pprint, cbapi
+import datetime, sys, pprint, cbapi, requests
 from cli_parser import build_cli_parser
-import requests
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings() 
     
-def run_query(args):
+def parent_search(args, pdoc):
+    
+    args.query = "hostname: %s process_name: %s process_pid: %d" % (pdoc['hostname'], pdoc['parent_name'], pdoc['parent_pid'])
+    
     # build a cbapi object
     cb = cbapi.CbApi(args.url, token=args.token, ssl_verify=args.ssl_verify)
 
     # use the cbapi object to iterate over all matching process documents
-    r = cb.process_search_iter(args.query)
-    return r
-
+    r = cb.process_search(args.query)
+    identifier = r['results'][0]['id']
+    seg_id = r['results'][0]['segment_id']
+    
+    events = cb.process_events(identifier, seg_id)
+    for cpe in events['process']['childproc_complete']: 
+        cpe_split = cpe.split('|',)
+        if int(cpe_split[4]) == pdoc['process_pid'] and cpe_split[5] == 'false':
+            process_end_time = datetime.datetime.strptime(cpe_split[0], "%Y-%m-%d %H:%M:%S.%f")
+            return process_end_time
+    return False
+    
 def main():
     args = build_cli_parser()
     yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
     yesterday = yesterday.strftime('%Y-%m-%d')
-    args.query = 'process_name:powershell.exe start: %s' % yesterday
-    answer = run_query(args)
+    args.query = 'process_name:powershell.exe and start: %s' % yesterday
+    print "Initial Query: %s", args.query
+    # build a cbapi object
+    cb = cbapi.CbApi(args.url, token=args.token, ssl_verify=args.ssl_verify)
+
+    # use the cbapi object to iterate over all matching process documents
+    answer = cb.process_search_iter(args.query)
     count = 0 
     lrcount = 0
     # iterate over each process document in the results set
     for pdoc in answer:
         count += 1
+        # Query the parent process to see if this child process has ended and assign the end date to process_end_time
+        process_end_time = parent_search(args, pdoc)
+        
+        if process_end_time:
+            end = process_end_time
+        else:
+            end = datetiem.datetime.strptime(pdoc['last_update'], "%Y-%m-%dT%H-%M-%S.%fz")
+       
         # Start time
         start = datetime.datetime.strptime(pdoc['start'], "%Y-%m-%dT%H:%M:%S.%fZ")
-        # Last Update time
-        ludate = datetime.datetime.strptime(pdoc['last_update'], "%Y-%m-%dT%H:%M:%S.%fZ")
-        # Difference betweeen the two
-        runtime = int((ludate - start).seconds)
+
+        # Difference betweeen the process end time and process start time
+        runtime = int((end - start).seconds)
+        
         # Change the compared value if 60 seconds is not considered a long run of powershell
         if runtime > 60:
             lrcount += 1
@@ -41,6 +64,8 @@ def main():
             print "Process Name: ", pdoc['process_name']
             print "Command Line: ", pdoc['cmdline']
             print "Runtime: %d seconds" % runtime
+            print "Process start  : %s" % start
+            print "Process endtime: %s" % process_end_time
             print "$$$$$$$$$$$$$$$$$$$$$$$$$"
     print "Matching Process Count: ", count
     print "Matching Long Running Process Count: ", lrcount
