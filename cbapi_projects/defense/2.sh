8c68734ca44158c2ab060b68d71a82c69db4b5c2
BJ Swope <bigblueswope+github+public@gmail.com>
2017-05-03 17:07:51 -0400

    none

--- cbapi_projects/defense/2.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-curl -o 2.txt -H X-Auth-Token:3HCVKVWUUVZPK1A8RUBFNAZ6/R21LPB37UC  https://api5.conferdeploy.net/integrationServices/v3/event?sha256hash=6c05e11399b7e3c8ed31bae72014cf249c144a8f4a2c54a758eb2e6fad47aec7&rows=1
