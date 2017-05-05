c1cf6794d7267781dd45e2f0261d5caa8d9316c1
BJ Swope <bigblueswope+github+public@gmail.com>
2017-05-04 20:52:19 -0400

    updated push_n_run

--- cbapi_projects/response/push_n_run.py
+++ cbapi_projects/response/push_n_run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import time
 from cbapi.response import CbEnterpriseResponseAPI, Sensor
 from cbapi.response.live_response_api import LiveResponseError
 import datetime
@@ -13,15 +14,21 @@ print("Enter Sensor ID")
 sensor_id = raw_input()
 print "Setting Sensor"
 sensor = c.select(Sensor, sensor_id)
- 
-print "Establishing LR session to sensor"
+
+print "Establishing LR session to sensor %s (%s)" % (sensor.computer_dns_name, sensor.computer_name)
+
 with sensor.lr_session() as session:	# this will wait until the Live Response session is established
 	try:
 		print "Pushing file to sensor."
-		session.put_file(open("test.bat", "r"), "test.bat")
-	except LiveResponseError as e:
-		if e.win32_error == 0x80070050:
-			file_info = session.list_directory("test.bat")
+		# read collect_system_info.bat and write it to the destination host
+		# if no paths are provided
+		#  the source file must be in the same directory as this script
+		#  and the destination file will land in CB's default directory
+		#  on Windows that is C:\Windows\CarbonBlack\
+		session.put_file(open("collect_system_info.bat", "r"), "collect_system_info.bat")
+	except LiveResponseError as e: 
+		if e.win32_error == 0x80070050: # Win32 Error Code meaning the file already exists
+			file_info = session.list_directory("collect_system_info.bat")
 			lmod = datetime.datetime.fromtimestamp(int(file_info[0]['last_write_time'])).strftime('%Y-%m-%d %H:%M:%S')
 			print "File exists. Last Modified: %s" % (lmod)
 			print "Keep existing or Replace with new version: (k/r)"
@@ -31,8 +38,8 @@ with sensor.lr_session() as session:	# this will wait until the Live Response se
 					print "Keeeping existing file."
 				elif kflag[0] in ['R', 'r']:
 					print "Deleting existing and re-pushing new version."
-					session.delete_file("test.bat")
-					session.put_file(open("test.bat", "r"), "test.bat")
+					session.delete_file("collect_system_info.bat")
+					session.put_file(open("collect_system_info.bat", "r"), "collect_system_info.bat")
 				else:
 					print "Response did not begin with either K or R.  Exiting program."
 					sys.exit(1)
@@ -42,5 +49,9 @@ with sensor.lr_session() as session:	# this will wait until the Live Response se
 		else:
 			raise
 	print "Running command"
-	print session.create_process("cmd.exe /c test.bat")	# output has the stdout from running c:\test.bat
+	session.create_process("cmd.exe /c collect_system_info.bat")	# output has the stdout from running collect_system_info.bat
+	print "Sleeping 15 seconds before retrieving the output."
+	time.sleep(15)
+	results = session.get_file("foo2.txt")
+	print results
 
